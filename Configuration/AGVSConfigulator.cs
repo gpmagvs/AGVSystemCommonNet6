bff51ef0d15187227be43cf381063731eb0c4fa4
jinweihuang <jinweihuang2033@outlook.com>
2023-09-21 09:07:36 +0800

    前端獲取參數前先從本機Reload參數

--- Configuration/AGVSConfigulator.cs
+++ Configuration/AGVSConfigulator.cs
@@ -9,12 +9,19 @@ namespace AGVSystemCommonNet6.Configuration
 {
     public class AGVSConfigulator
     {
+        public static string _configFilePath { get; private set; } = "";
+        public static SystemConfigs SysConfigs { get; set; }
+        public static void LoadConfig()
+        {
+            SysConfigs = LoadConfig(_configFilePath);
+        }
         public static SystemConfigs LoadConfig(string configFilePath)
         {
+            _configFilePath = configFilePath;
             SystemConfigs systemConfigs = new SystemConfigs();
             if (File.Exists(configFilePath))
             {
-                systemConfigs= JsonConvert.DeserializeObject<SystemConfigs>(File.ReadAllText(configFilePath));
+                systemConfigs = JsonConvert.DeserializeObject<SystemConfigs>(File.ReadAllText(configFilePath));
             }
             else
             {
@@ -23,7 +30,6 @@ namespace AGVSystemCommonNet6.Configuration
             File.WriteAllText(configFilePath, JsonConvert.SerializeObject(systemConfigs, Formatting.Indented));
             return systemConfigs;
         }
-        public static SystemConfigs SysConfigs { get; set; }
         public static void Init()
         {
             SysConfigs = LoadConfig("C:\\AGVS\\SystemConfigs.json");
