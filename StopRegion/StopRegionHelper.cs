5592e201fcc24a030a5f2d370dee49f4f39d71ad
kenwu <kenwu@gpmcorp.com.tw>
2023-11-08 13:41:18 +0800

    新增StopRegion的資料庫等等的

---
+++ StopRegion/StopRegionHelper.cs
@@ -0,0 +1,84 @@
+﻿using AGVSystemCommonNet6.Availability;
+using AGVSystemCommonNet6.Configuration;
+using AGVSystemCommonNet6.DATABASE.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AGVSystemCommonNet6.clsEnums;
+
+namespace AGVSystemCommonNet6.StopRegion
+{
+    public class StopRegionHelper
+    {
+        public clsStopRegionDto stopRegionDto_IdleEvent = new clsStopRegionDto();
+        private MAIN_STATUS PreviousMainStatus_IdleEvent =  MAIN_STATUS.Unknown;
+        
+        public StopRegionHelper(string AGVName)
+        {
+            stopRegionDto_IdleEvent = new clsStopRegionDto();
+            stopRegionDto_IdleEvent.AGVName = AGVName;
+        }
+
+        public void UpdateStopRegionData(MAIN_STATUS CurrentStatus,string Position)
+        {
+            try
+            {
+                if (PreviousMainStatus_IdleEvent != CurrentStatus)
+                {
+                    if (CurrentStatus == MAIN_STATUS.IDLE) //從其他狀態變成Idle
+                    {
+                        stopRegionDto_IdleEvent.StartTime = DateTime.Now;
+                        stopRegionDto_IdleEvent.RegionName = Position;
+                        stopRegionDto_IdleEvent.Main_Status = MAIN_STATUS.IDLE;
+                        stopRegionDto_IdleEvent.EndTime = DateTime.Now;
+                        UpdateStopRegionDataToDataBase(stopRegionDto_IdleEvent);
+                    }
+                    else if (PreviousMainStatus_IdleEvent ==  MAIN_STATUS.IDLE) //從Idle變成其他狀態
+                    {
+                        stopRegionDto_IdleEvent.EndTime = DateTime.Now;
+                        UpdateStopRegionDataToDataBase(stopRegionDto_IdleEvent);
+                    }
+                    PreviousMainStatus_IdleEvent = CurrentStatus;
+                }
+                else
+                {
+                    if (CurrentStatus == MAIN_STATUS.IDLE)
+                    {
+                        stopRegionDto_IdleEvent.EndTime = DateTime.Now;
+                        UpdateStopRegionDataToDataBase(stopRegionDto_IdleEvent);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async void UpdateStopRegionDataToDataBase(clsStopRegionDto clsStopRegion)
+        {
+            try
+            {
+                using (DbContextHelper aGVSDbContext = new DbContextHelper(AGVSConfigulator.SysConfigs.DBConnection))
+                {
+                    var currentData = aGVSDbContext._context.StopRegionData.FirstOrDefault(data => data.AGVName == clsStopRegion.AGVName && data.StartTime == clsStopRegion.StartTime);
+                    if (currentData == null)
+                    {
+                        aGVSDbContext._context.StopRegionData.Add(clsStopRegion);
+                    }
+                    else
+                    {
+                        currentData.EndTime = clsStopRegion.EndTime;
+                    }
+                    var ret = aGVSDbContext._context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{typeof(AGVStatusDBHelper).Name} " + ex.Message);
+            }
+        }
+    }
+}
---
+++ StopRegion/clsStopRegionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AGVSystemCommonNet6.clsEnums;
+
+namespace AGVSystemCommonNet6.StopRegion
+{
+    public class clsStopRegionDto
+    {
+        [Key]
+        public DateTime StartTime { get; set; }
+        public string AGVName { get; set; } = "";
+        public DateTime EndTime { get; set; }
+        public string RegionName { get; set; } = "";
+        public MAIN_STATUS Main_Status { get; set; }
+    }
+}
