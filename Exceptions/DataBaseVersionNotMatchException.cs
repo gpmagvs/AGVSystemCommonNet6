8cb2349aa625ea2777be453571dcdc8aa4443c2b
jinweihuang <jinweihuang2033@outlook.com>
2024-02-07 18:14:16 +0800

    新增 DataBaseVersionNotMatchException.cs

---
+++ Exceptions/DataBaseVersionNotMatchException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace AGVSystemCommonNet6.Exceptions
+{
+    [Serializable]
+    internal class DataBaseVersionNotMatchException : Exception
+    {
+        public DataBaseVersionNotMatchException()
+        {
+        }
+
+        public DataBaseVersionNotMatchException(string? message) : base(message)
+        {
+        }
+
+        public DataBaseVersionNotMatchException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DataBaseVersionNotMatchException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
