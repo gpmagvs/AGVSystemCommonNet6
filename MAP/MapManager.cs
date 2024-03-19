50207b12fd47a394550c71dddcb7570723b4d77b
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-03-19 11:47:34 +0800

    地圖檔載入時若有null數值則忽略

--- MAP/MapManager.cs
+++ MAP/MapManager.cs
@@ -24,7 +24,10 @@ namespace AGVSystemCommonNet6.MAP
                 return null;
             try
             {
-                var data_obj = JsonConvert.DeserializeObject<Dictionary<string, Map>>(json);
+                var data_obj = JsonConvert.DeserializeObject<Dictionary<string, Map>>(json, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
                 var map = data_obj["Map"];
                 if (auto_create_segment && map.Points.Count != 0 && map.Segments.Count == 0)
                 {
