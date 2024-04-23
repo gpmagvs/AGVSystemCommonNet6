0d562c90aa9e7e8f1c3e78283e14103838ae91a9
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-04-23 16:42:48 +0800

    add Services/EquipmentStateRequest.cs

---
+++ GPMRosMessageNet/Services/EquipmentStateRequest.cs
@@ -0,0 +1,50 @@
+﻿using RosSharp.RosBridgeClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGVSystemCommonNet6.GPMRosMessageNet.Services
+{
+    public class EquipmentStateRequest : Message
+    {
+        public const string RosMessageName = "gpm_msgs/EquipmentState";
+        public string equipment { get; set; }
+        public string state { get; set; }
+        public EquipmentStateRequest()
+        {
+            equipment = "";
+            state = "on";
+        }
+        public EquipmentStateRequest(EQUIPMENTS equipment, bool state)
+        {
+            this.equipment = equipment.ToString();
+            this.state = state ? "on" : "off";
+        }
+
+        public enum EQUIPMENTS
+        {
+            instrument,
+            illuminance,
+            decibel,
+            temperature,
+            humidity,
+            PID
+        }
+    }
+
+    public class EquipmentStateResponse : Message
+    {
+        public const string RosMessageName = "gpm_msgs/EquipmentState";
+        public bool confirm { get;set; }
+        public EquipmentStateResponse()
+        {
+            confirm=false;
+        }
+        public EquipmentStateResponse(bool confirm)
+        {
+            this.confirm = confirm;
+        }
+    }
+}
---
+++ GPMRosMessageNet/Services/SensorStateRequest.cs
@@ -0,0 +1,46 @@
+﻿using RosSharp.RosBridgeClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGVSystemCommonNet6.GPMRosMessageNet.Services
+{
+    public class SensorStateStateRequest : Message
+    {
+        public const string RosMessageName = "gpm_msgs/SensorState";
+        public string equipment { get; set; }
+        public string state { get; set; }
+        public SensorStateStateRequest()
+        {
+            equipment = "";
+            state = "on";
+        }
+        public SensorStateStateRequest(SENSORS equipment, bool state)
+        {
+            this.equipment = equipment.ToString();
+            this.state = state ? "on" : "off";
+        }
+
+        public enum SENSORS
+        {
+            Ultrasonic,
+            GroundHoleCamera
+        }
+    }
+
+    public class SensorStateStateResponse : Message
+    {
+        public const string RosMessageName = "gpm_msgs/SensorState";
+        public bool confirm { get; set; }
+        public SensorStateStateResponse()
+        {
+            confirm = false;
+        }
+        public SensorStateStateResponse(bool confirm)
+        {
+            this.confirm = confirm;
+        }
+    }
+}
