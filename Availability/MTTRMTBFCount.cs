e1062cb17aed1d8ca81a42991caccdbfef384f8c
YouLin <karta083729@gmail.com>
2024-04-30 18:11:41 +0800

    修正MTTR統計功能

--- Availability/MTTRMTBFCount.cs
+++ Availability/MTTRMTBFCount.cs
@@ -11,38 +11,45 @@ namespace AGVSystemCommonNet6.Availability
 {
     public class MTTRMTBFCount
     {
-        public static int [] Mttr_data;
+        public static List<int> Mttr_data = new List<int>();
+        public static List<DateTime> Mttr_date = new List<DateTime>();
+
         public static void MTTR_TimeCount(DateTime startTime, DateTime endTime, string AGV_Name)
         {
-            int i = 1;
-            int count = 0;
-            int Mttr_DurationCount =0;
-            using (var dbhelper = new DbContextHelper(AGVSConfigulator.SysConfigs.DBConnection))
+            Mttr_date.Clear();
+            Mttr_data.Clear();
+            int count = 1;
+            int Mttr_DurationCount = 0;
+            List<clsAlarmDto> alarms = new List<clsAlarmDto>();
+            for (DateTime time = startTime; time <= endTime; time.AddDays(1))
             {
-                DateTime time ;
-                List<clsAlarmDto> alarms = new List<clsAlarmDto>();
-                for (time = startTime; time <= endTime; time.AddDays(1))
+                
+                using (var dbhelper = new DbContextHelper(AGVSConfigulator.SysConfigs.DBConnection))
                 {
                     alarms = new List<clsAlarmDto>();
                     var _alarms = dbhelper._context.Set<clsAlarmDto>().Where(alarm => alarm.Time >= time && alarm.Time <= time.AddDays(1)
                                         && (AGV_Name == "AGV_001" ? (true) : (alarm.Equipment_Name == AGV_Name))
                     );
                     count = _alarms.Count();
-                    List<string> MTTR_Time = _alarms.Select(alarm => $"{alarm.Time}").ToList();
+                    if (count == 0) 
+                    { 
+                        count = 1; 
+                    }
                     List<string> MTTR_Duration = _alarms.Select(alarm => $"{alarm.Duration}").ToList();
                     List<int> MTTR = new List<int>();
                     foreach (string str in MTTR_Duration)
                     {
                         MTTR.Add(int.Parse(str));
                     }
-
                     foreach (int num in MTTR)
                     {
                         Mttr_DurationCount += num;
                     }
-                    i++;
-                    Mttr_data[i] = Mttr_DurationCount / count;
+                    Mttr_data.Add(Mttr_DurationCount / count);
+                    Mttr_date.Add(time);
+                    time = time.AddDays(1);
                 }
+                
             }
         }
         public static void MTBF_TimeCount(DateTime startTime, DateTime endTime, string AGV_Name)
