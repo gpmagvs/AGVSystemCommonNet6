22f534af03e91fd6ee65962211109f3445b68f5b
jinweihunag <jinweihuang@gpmcorp.com.tw>
2024-05-09 07:15:44 +0800

    新增背景服務將資料庫數據緩存到記憶體中/ clsAGVStateDto 將部分NotMapped設定移除

---
+++ DATABASE/BackgroundServices/DatabaseBackgroundService.cs
@@ -0,0 +1,70 @@
+﻿using AGVSystemCommonNet6.DATABASE;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+
+namespace AGVSystemCommonNet6.DATABASE.BackgroundServices
+{
+    /// <summary>
+    /// DatabaseBackgroundService
+    /// </summary>
+    public class DatabaseBackgroundService : IHostedService, IDisposable
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private Timer _timer;
+
+        public DatabaseBackgroundService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            // 設置定時器，每隔 10 秒執行一次
+            _timer = new Timer(async _ => await DoWork(), null, TimeSpan.Zero, TimeSpan.FromMilliseconds(150));
+            return Task.CompletedTask;
+        }
+
+        private async Task DoWork()
+        {
+            try
+            {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    // 從 DI 容器中獲取 DbContext 或其他服務
+                    AGVSDbContext context = scope.ServiceProvider.GetRequiredService<AGVSDbContext>();
+                    // 這裡進行背景數據庫操作或其他業務邏輯
+                    var customerCount = context.Tasks.Count();
+                    DatabaseCaches.TaskCaches.WaitExecuteTasks = await context.Tasks.Where(task => task.State == AGVDispatch.Messages.TASK_RUN_STATUS.WAIT).AsNoTracking().ToListAsync();
+                    DatabaseCaches.TaskCaches.CompleteTasks = await context.Tasks.OrderByDescending(task => task.RecieveTime)
+                                                                           .Where(task => task.State == AGVDispatch.Messages.TASK_RUN_STATUS.CANCEL || task.State == AGVDispatch.Messages.TASK_RUN_STATUS.ACTION_FINISH)
+                                                                           .Take(50).AsNoTracking().ToListAsync();
+                    DatabaseCaches.TaskCaches.RunningTasks = await context.Tasks.Where(task => task.State == AGVDispatch.Messages.TASK_RUN_STATUS.NAVIGATING).AsNoTracking().ToListAsync();
+
+                    DatabaseCaches.Alarms.UnCheckedAlarms = await context.SystemAlarms.Where(alarm => !alarm.Checked).AsNoTracking().ToListAsync();
+
+                    DatabaseCaches.Vehicle.VehicleStates = await context.AgvStates.AsNoTracking().ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[DatabaseBackgroundService] DoWork Exception" + ex.Message);
+            }
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+
+}
--- clsAGVStateDto.cs
+++ clsAGVStateDto.cs
@@ -57,11 +57,8 @@ namespace AGVSystemCommonNet6
         [MaybeNull]
         public double VehicleWidth { get; set; } = 70;
 
-        [NotMapped]// 此欄位不會寫入資料表
         public string TaskSourceStationName { get; set; } = "AS";
-        [NotMapped]// 此欄位不會寫入資料表
         public string TaskDestineStationName { get; set; } = "BB";
-        [NotMapped]// 此欄位不會寫入資料表
         public string StationName { get; set; } = "";
         [MaybeNull]
         public string IP { get; set; } = "127.0.0.1";
@@ -103,6 +100,11 @@ namespace AGVSystemCommonNet6
             LowBatLvThreshold = entity.LowBatLvThreshold;
             MiddleBatLvThreshold = entity.MiddleBatLvThreshold;
             HighBatLvThreshold = entity.HighBatLvThreshold;
+            TaskSourceStationName = entity.TaskSourceStationName;
+            TaskDestineStationName = entity.TaskDestineStationName;
+            StationName = entity.StationName;
+
+
         }
 
         public bool HasChanged(clsAGVStateDto newState)
