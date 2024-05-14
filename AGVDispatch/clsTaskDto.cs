9657c1c6a4d621d2be4397f9d51aef3eb672d209
haochang <haochang@gpmcorp..com.tw>
2024-05-14 09:46:56 +0800

    clsTaskDto 加入CST_TYPE

--- AGVDispatch/clsTaskDto.cs
+++ AGVDispatch/clsTaskDto.cs
@@ -1,4 +1,5 @@
 ﻿using AGVSystemCommonNet6.AGVDispatch.Messages;
+using AGVSystemCommonNet6.Configuration;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -163,6 +164,8 @@ namespace AGVSystemCommonNet6.AGVDispatch
 
         [Required]
         public string To_Slot { get; set; } = "-1";
+
+        public int CST_TYPE = -1;
 
         public string Carrier_ID { get; set; } = "";
 
