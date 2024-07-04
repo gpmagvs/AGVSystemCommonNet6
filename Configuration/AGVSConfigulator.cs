d05669f42d2d734a927f62f76d0935e5d606dfbf
jinweihuang20 <jinweihuang1020@gmail.com>
2024-07-04 10:18:36 +0800

    新增系統參數/監視參數檔變化載入

--- Configuration/AGVSConfigulator.cs
+++ Configuration/AGVSConfigulator.cs
@@ -12,13 +12,15 @@ namespace AGVSystemCommonNet6.Configuration
         public static string ConfigsFilesFolder { get; set; } = @"C:\AGVS";
         public static string _configFilePath { get; private set; } = "";
         public static SystemConfigs SysConfigs { get; set; }
+
+        private static FileSystemWatcher SysConfigFileWatcher;
+
         public static void LoadConfig()
         {
             SysConfigs = LoadConfig(_configFilePath);
         }
         public static SystemConfigs LoadConfig(string configFilePath)
         {
-            _configFilePath = configFilePath;
             SystemConfigs systemConfigs = new SystemConfigs();
             if (File.Exists(configFilePath))
             {
@@ -28,12 +30,35 @@ namespace AGVSystemCommonNet6.Configuration
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(configFilePath));
             }
-            File.WriteAllText(configFilePath, JsonConvert.SerializeObject(systemConfigs, Formatting.Indented));
+            string json = JsonConvert.SerializeObject(systemConfigs, Formatting.Indented);
+            File.WriteAllText(configFilePath, json);
+            Console.WriteLine(json);
             return systemConfigs;
         }
         public static void Init()
         {
-            SysConfigs = LoadConfig(Path.Combine(ConfigsFilesFolder, "SystemConfigs.json"));
+            _configFilePath = Path.Combine(ConfigsFilesFolder, "SystemConfigs.json");
+            SysConfigs = LoadConfig(_configFilePath);
+            WatchSystemConfigs(_configFilePath);
+        }
+
+        private static void WatchSystemConfigs(string _configFilePath)
+        {
+            SysConfigFileWatcher = new FileSystemWatcher(Path.GetDirectoryName(_configFilePath), Path.GetFileName(_configFilePath));
+            SysConfigFileWatcher.Changed += Watcher_Changed;
+            SysConfigFileWatcher.EnableRaisingEvents = true;
+        }
+
+        private static async void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            SysConfigFileWatcher.EnableRaisingEvents = false;
+            //copy the file as temp file and load it
+            string _tempConfigFilePath = _configFilePath + $"{DateTime.Now.Ticks}.tmp";
+            File.Copy(_configFilePath, _tempConfigFilePath, true);
+            SysConfigs = JsonConvert.DeserializeObject<SystemConfigs>(File.ReadAllText(_tempConfigFilePath));
+            File.Delete(_tempConfigFilePath);
+            await Task.Delay(100);
+            SysConfigFileWatcher.EnableRaisingEvents = true;
         }
 
         internal static void Save(SystemConfigs config)
