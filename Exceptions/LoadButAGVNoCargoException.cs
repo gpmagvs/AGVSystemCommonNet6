9587f438f8afd70f00a791777fd6b911e750fe3d
jinweihuang20 <jinweihuang1020@gmail.com>
2024-07-18 18:24:15 +0800

    新增派車使用的例外物件

---
+++ Exceptions/LoadButAGVNoCargoException.cs
@@ -0,0 +1,16 @@
+﻿using AGVSystemCommonNet6.Alarm;
+
+namespace AGVSystemCommonNet6.Exceptions
+{
+    public class LoadButAGVNoCargoException : VMSExceptionAbstract
+    {
+        public override string Message => "AGV車上沒有貨物時不可執行放貨任務";
+        public LoadButAGVNoCargoException() : base()
+        {
+        }
+        public LoadButAGVNoCargoException(string message) : base(message)
+        {
+        }
+        public override ALARMS Alarm_Code { get; set; } = ALARMS.CANNOT_DISPATCH_LOAD_TASK_WHEN_AGV_NO_CARGO;
+    }
+}
