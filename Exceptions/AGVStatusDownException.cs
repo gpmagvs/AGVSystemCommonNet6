163c4896a9fde3e570408e19c9f72a679cc133fb
jinweihuang20 <jinweihuang1020@gmail.com>
2024-07-19 15:02:20 +0800

    新增 EnvironmentVariables/AGVStatusDownException

---
+++ Exceptions/AGVStatusDownException.cs
@@ -0,0 +1,16 @@
+﻿using AGVSystemCommonNet6.Alarm;
+
+namespace AGVSystemCommonNet6.Exceptions
+{
+    public class AGVStatusDownException : VMSExceptionAbstract
+    {
+        public override string Message => "AGV狀態異常(DOWN)";
+        public AGVStatusDownException() : base()
+        {
+        }
+        public AGVStatusDownException(string message) : base(message)
+        {
+        }
+        public override ALARMS Alarm_Code { get; set; } = ALARMS.AGV_STATUS_DOWN;
+    }
+}
