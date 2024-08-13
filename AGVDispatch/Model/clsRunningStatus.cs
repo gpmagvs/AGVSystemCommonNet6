3b8c50ade83e2e339f61ee03aa1039a9d029f668
jinweihuang20 <jinweihuang1020@gmail.com>
2024-08-13 15:42:55 +0800

    clsCoordination 複寫 ToString 方法 print (X,Y,Theta)

--- AGVDispatch/Model/clsRunningStatus.cs
+++ AGVDispatch/Model/clsRunningStatus.cs
@@ -98,6 +98,11 @@ namespace AGVSystemCommonNet6.AGVDispatch.Model
         public double X { get; set; }
         public double Y { get; set; }
         public double Theta { get; set; }
+
+        public override string ToString()
+        {
+            return $"({X},{Y},{Theta})";
+        }
     }
     public class clsAlarmCode
     {
