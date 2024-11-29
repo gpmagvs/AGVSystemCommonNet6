f7e3c5e15ca6297c60880ce46a39c4e35d395370
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-29 16:08:21 +0800

    Merge and remove old code line

--- Microservices/MCSCIM/MCSCIMService.cs
+++ Microservices/MCSCIM/MCSCIMService.cs
@@ -122,39 +122,33 @@ namespace AGVSystemCommonNet6.Microservices.MCS
         public static async Task<(bool confirm, string message)> ShelfStatusChange(object wipData)
         {
             (bool confirm, string message) response = new(false, "[MCSCIMService.AlarmReporter] System Error.");
-            using (agvs_http)
-            {
                 try
                 {
                     var route = $"/api/HostMode/ShelfStatusChange";
-                    (bool success, string json) v = await agvs_http.PostAsync(route, wipData);
+                    (bool success, string json) v = await _http.PostAsync(route, wipData);
                     response.confirm = v.success;
                     response.message = v.json;
                 }
                 catch (Exception ex)
                 {
-                    response.message = $"[MCSCIMService.AlarmReporter] Report to: {agvs_http.http_client.BaseAddress} with exmessage: {ex.Message}";
+                    response.message = $"[MCSCIMService.AlarmReporter] Report to: {_http.http_client.BaseAddress} with exmessage: {ex.Message}";
                 }
-            }
             return response;
         }
         public static async Task<(bool confirm, string message)> ZoneCapacityChange(object wipData)
         {
             (bool confirm, string message) response = new(false, "[MCSCIMService.AlarmReporter] System Error.");
-            using (agvs_http)
-            {
                 try
                 {
                     var route = $"/api/HostMode/ZoneCapacityChange";
-                    (bool success, string json) v = await agvs_http.PostAsync(route, wipData);
+                    (bool success, string json) v = await _http.PostAsync(route, wipData);
                     response.confirm = v.success;
                     response.message = v.json;
                 }
                 catch (Exception ex)
                 {
-                    response.message = $"[MCSCIMService.AlarmReporter] Report to: {agvs_http.http_client.BaseAddress} with exmessage: {ex.Message}";
+                    response.message = $"[MCSCIMService.AlarmReporter] Report to: {_http.http_client.BaseAddress} with exmessage: {ex.Message}";
                 }
-            }
             return response;
         }
         internal class ResponseObject
