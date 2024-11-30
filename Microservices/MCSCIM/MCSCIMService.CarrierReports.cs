0319a546ec91c089c89750e40e24ac7908191a2a
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-30 19:48:55 +0800

    MCSCIMService add 'ZoneData' Dto model

--- Microservices/MCSCIM/MCSCIMService.CarrierReports.cs
+++ Microservices/MCSCIM/MCSCIMService.CarrierReports.cs
@@ -14,13 +14,45 @@ namespace AGVSystemCommonNet6.Microservices.MCS
     /// </summary>
     public partial class MCSCIMService
     {
-        public static async Task<(bool confirm, string message)> ShelfStatusChange(object wipData)
+
+        public class ZoneData
+        {
+            public string ZoneName { get; set; } = string.Empty;
+            public int ZoneCapacity => LocationStatusList.Count(p => p.DisabledStatus == 0 && !p.IsCargoExist);
+            public int ZoneTotalSize => LocationStatusList.Count;
+
+            /// <summary>
+            /// 0: Shelf,  1: Port,  2: Other,  9: HandOff
+            /// </summary>
+            public int ZoneType { get; set; } = 0;
+
+            public List<LocationStatus> LocationStatusList { get; set; } = new List<LocationStatus>();
+
+
+            public class LocationStatus
+            {
+                public string ShelfId { get; set; } = "ShelfID";
+                public string CarrierID { get; set; } = string.Empty;
+                public bool IsCargoExist { get; set; } = false;
+                /// <summary>
+                /// 0:enable,1:disabled;
+                /// </summary>
+                public int DisabledStatus { get; set; } = 0;
+                /// <summary>
+                /// 0= None/Dynamic(無限制) ;1 = Normal(DCS可派送，MCS異常處理不可派送);2 = Abnormal(DCS不可派送，MCS異常處理可派送)
+                /// </summary>
+                public int ProcessState { get; set; } = 0;
+            }
+
+        }
+
+        public static async Task<(bool confirm, string message)> ShelfStatusChange(List<ZoneData> zoneData)
         {
             (bool confirm, string message) response = new(false, "[MCSCIMService.AlarmReporter] System Error.");
             try
             {
                 var route = $"/api/HostMode/ShelfStatusChange";
-                (bool success, string json) v = await _http.PostAsync(route, wipData);
+                (bool success, string json) v = await _http.PostAsync(route, zoneData);
                 response.confirm = v.success;
                 response.message = v.json;
             }
@@ -57,7 +89,7 @@ namespace AGVSystemCommonNet6.Microservices.MCS
         /// <returns></returns>
         public static async Task CarrierInstallCompletedReport(string CarrierID, string CarrierLoc, string CarrierZoneName, ushort HandoffType)
         {
-            await _http.PostAsync<object, object>($"/api/Carrier/CarrierInstallCompleted?CarrierID={CarrierID}&CarrierLoc={CarrierLoc}&CarrierZoneName={CarrierZoneName}&HandoffType={HandoffType}",null,8);
+            await _http.PostAsync<object, object>($"/api/Carrier/CarrierInstallCompleted?CarrierID={CarrierID}&CarrierLoc={CarrierLoc}&CarrierZoneName={CarrierZoneName}&HandoffType={HandoffType}", null, 8);
         }
         /// <summary>
         /// [CEID=152]
@@ -69,7 +101,7 @@ namespace AGVSystemCommonNet6.Microservices.MCS
         /// <returns></returns>
         public static async Task CarrierRemoveCompletedReport(string CarrierID, string CarrierLoc, string CarrierZoneName, ushort HandoffType)
         {
-            await _http.PostAsync<object, object>($"/api/Carrier/CarrierRemoveCompleted?CarrierID={CarrierID}&CarrierLoc={CarrierLoc}&CarrierZoneName={CarrierZoneName}&HandoffType={HandoffType}",null,8);
+            await _http.PostAsync<object, object>($"/api/Carrier/CarrierRemoveCompleted?CarrierID={CarrierID}&CarrierLoc={CarrierLoc}&CarrierZoneName={CarrierZoneName}&HandoffType={HandoffType}", null, 8);
         }
     }
 }
