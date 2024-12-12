9268d7bbcb78f8abb21a847990f441a397f13f62
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-12 09:30:53 +0800

    AGVSDbContext 新增確認 table是否為 lock狀態的方法

--- DATABASE/AGVSDbContext.cs
+++ DATABASE/AGVSDbContext.cs
@@ -78,5 +78,77 @@ namespace AGVSystemCommonNet6.DATABASE
         {
             base.OnModelCreating(modelBuilder);
         }
+
+        public bool IsAgvStatesTableLocking()
+        {
+            return IsTableLocking("AgvStates");
+        }
+
+        public bool IsTaskTableLocking()
+        {
+            return IsTableLocking("Tasks");
+        }
+
+        public bool IsAlarmTableLocking()
+        {
+            return IsTableLocking("SystemAlarms");
+        }
+
+        private bool IsTableLocking(string tableName)
+        {
+            var result = new List<LockInfo>();
+            string sql = @"
+                            SELECT request_session_id AS spid,
+                                resource_type AS rt,
+                                resource_databASe_id AS rdb,
+                                (CASE resource_type 
+                                WHEN 'OBJECT' then object_name(resource_ASsociated_entity_id) 
+                                ELSE 
+                                (SELECT object_name(object_id) FROM sys.partitions 
+                                    WHERE hobt_id = resource_ASsociated_entity_id) END) AS objname,
+                                resource_description AS rd,
+                                request_mode AS rm,
+                                request_status AS rs
+                            FROM sys.dm_tran_locks
+                            WHERE request_mode = 'X' OR request_mode = 'IX'";
+
+            using (var command = this.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = sql;
+
+                if (command.Connection.State != System.Data.ConnectionState.Open)
+                    command.Connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        result.Add(new LockInfo
+                        {
+                            Spid = reader.GetInt32(0),
+                            Rt = reader.GetString(1),
+                            Rdb = reader.GetInt32(2),
+                            Objname = reader.IsDBNull(3) ? null : reader.GetString(3),
+                            Rd = reader.IsDBNull(4) ? null : reader.GetString(4),
+                            Rm = reader.GetString(5),
+                            Rs = reader.GetString(6)
+                        });
+                    }
+                }
+            }
+            return result.Where(ret => ret.Objname == tableName).Any();
+        }
+
+        // 首先需要定義一個對應的模型類
+        public class LockInfo
+        {
+            public int Spid { get; set; }
+            public string Rt { get; set; }
+            public int Rdb { get; set; }
+            public string Objname { get; set; }
+            public string Rd { get; set; }
+            public string Rm { get; set; }
+            public string Rs { get; set; }
+        }
     }
 }
