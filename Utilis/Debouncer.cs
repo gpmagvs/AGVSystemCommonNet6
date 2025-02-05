81faf608e01b851e7c2c5093675624b61b63626b
jinweihuang20 <jinweihuang1020@gmail.com>
2025-02-05 10:38:23 +0800

    新增 Debouncer 類別及其 Debounce 方法
    接受 Action 和可選延遲時間。
    實現中處理 cts 的取消和釋放，並使用 Task.Delay 延遲執行 action。

---
+++ Utilis/Debouncer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGVSystemCommonNet6.Utilis
+{
+    public class Debouncer
+    {
+        private CancellationTokenSource cts;
+
+        /// <summary>
+        /// 防抖動
+        /// </summary>
+        /// <param name="action">動作</param>
+        /// <param name="delay">延遲時間(毫秒)</param>
+        public void Debounce(Action action, int delay = 300)
+        {
+            if (cts != null)
+            {
+                cts.Cancel();
+                cts.Dispose();
+            }
+            cts = new CancellationTokenSource();
+            Task.Delay(delay, cts.Token).ContinueWith((t) =>
+            {
+                if (!t.IsCanceled)
+                {
+                    action();
+                }
+            });
+        }
+    }
+}
